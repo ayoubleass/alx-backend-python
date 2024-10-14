a08949ddfc8647cbffcf1e1a10436d3e2452edb0
ayoubleass <ayoubleas@gmail.com>
2024-10-14 15:13:59 +0000

    Update : file 0-basic_async_syntax.py, 2-measure_runtime.py, 1-concurrent_coroutines.py, 3-tasks.py, 4-tasks.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,11 +2,12 @@
 """
 This module contains asynchronous routines to wait for random delays.
 """
+from typing import List
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Spawns wait_random n times with the specified max_delay
     and returns the list of all the delays (float values).
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -2,11 +2,12 @@
 """
 This module contains asynchronous routines to wait for random delays.
 """
+from typing import List
 import asyncio
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n, max_delay):
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Spawns wait_random n times with the specified max_delay
     and returns the list of all the delays (float values).
